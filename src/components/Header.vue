c399d61aa0e29507f8888204b8c8c15bcdd581bf
diegoOG09 <xdiegoemmax@outlook.com>
2023-01-19 21:21:11 -0600

    firebase setup

---
+++ src/components/Header.vue
@@ -0,0 +1,99 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="nav_title">
+        <img
+          id="nav_image"
+          src="../../public/PngItem_3542954.png"
+          alt="travel header icon"
+        />
+        <h1>Viajes Bernal</h1>
+      </div>
+      <div class="nav_items">
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Viajar</a></li>
+          <li><a href="#">Contacto</a></li>
+          <li><a href="#">Nosotros</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+      <div class="nav_login">
+        <a href="#">Iniciar sesión</a>
+        <a id="sign_up" href="#">Regístrate</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+header {
+  margin-bottom: 4rem;
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: $montserrat;
+    font-size: 1.5rem;
+    padding: 2rem 0 0 0;
+
+    .nav_title {
+      display: flex;
+      align-items: center;
+      width: 30%;
+      color: $white;
+      #nav_image {
+        filter: invert(95%);
+        width: 12%;
+        margin-right: 1rem;
+      }
+    }
+
+    .nav_items > ul {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 2.5rem;
+      list-style: none;
+      padding: 0;
+      a {
+        font-size: 1.4rem;
+        text-decoration: none;
+        text-shadow: 2px 1px 2px rgba(0, 0, 0, 0.6);
+        color: white;
+        font-weight: 500;
+        &:hover {
+          transition: 0.8s;
+        }
+      }
+    }
+    .nav_login {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 1.5rem;
+      a {
+        font-weight: 600;
+        text-decoration: none;
+        color: white;
+        text-shadow: 2px 1px 2px rgba(0, 0, 0, 0.6);
+      }
+      #sign_up {
+        background-color: $green;
+        text-shadow: none;
+        padding: 1rem 1rem;
+        border-radius: 8px;
+        color: $white;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        &:hover {
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,64 @@
+<template>
+  <main>
+    <h2 class="main_text container">
+      Viaja con nosotros y recorre los lugares más hermosos de México
+    </h2>
+    <p class="main_paragraph container">
+      México es un país bellísimo y con una diversidad inmensa tanto de fauna,
+      flora y ecosistemas, incluyendo vistas hermosas de playas, montañas,
+      desiertos y más. Es por eso que nos dedicamos a recorrer algunos de los
+      lugares más famosos de dicho país.
+    </p>
+    <a class="container" id="main_button" href="#">Apartar un lugar</a>
+  </main>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+main {
+  .main_text {
+    line-height: 5.5rem;
+    margin-bottom: 3rem;
+    padding-top: 10rem;
+    width: 75%;
+    color: $white;
+    font-family: $montserrat;
+    font-size: 4rem;
+    font-weight: 700;
+    text-align: center;
+    text-shadow: 2px 1px 2px rgba(0, 0, 0, 0.6);
+    letter-spacing: 1px;
+  }
+  .main_paragraph {
+    line-height: 2.5rem;
+    text-align: center;
+    width: 60%;
+    color: $white;
+    font-family: $montserrat;
+    font-size: 1.8rem;
+    font-weight: 400;
+    margin-bottom: 2rem;
+  }
+  #main_button {
+    font-weight: 700;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 15rem;
+    color: $white;
+    text-decoration: none;
+    font-family: $montserrat;
+    background-color: $green;
+    padding: 1rem 2.5rem;
+    border-radius: 8px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    &:hover {
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+    }
+  }
+}
+</style>
